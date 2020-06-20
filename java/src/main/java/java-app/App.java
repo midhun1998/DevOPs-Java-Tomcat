173f1e6fc523b92a91e33bb0cfe581cdfe460245
midhun1998 <midhun.rnair011@gmail.com>
2020-06-20 17:20:07 +0530

    Adding Maven App Files

---
+++ java/src/main/java/java-app/App.java
@@ -0,0 +1,13 @@
+package java-app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to my CI/CD pipelining using Jenkins." );
+    }
+}
